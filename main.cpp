a1e24bf304d2ac63b563237d28683726a124399a
翼 茂木 <k022g0130@m.neec.ac.jp>
2023-04-24 02:45:34 +0900

    dllファイル無し。手詰まり

--- main.cpp
+++ main.cpp
@@ -38,8 +38,11 @@ std::string ConvertString(const std::wstring& str);
 
 //CompilerShader関数
 IDxcBlob* compilerShader(
+	//CompilerするShaderファイルへのパス
 	const std::wstring& filePath,
+	//Compilerに使用するProfile
 	const wchar_t* profile,
+	//初期化で生成したものを３つ
 	IDxcUtils* dxcUtils,
 	IDxcCompiler3* dxcCompiler,
 	IDxcIncludeHandler* includeHandler);

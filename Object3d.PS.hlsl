d67aedb29a037969c7c183214dbde1a5a7753fc4
翼 茂木 <k022g0130@m.neec.ac.jp>
2023-04-25 19:08:34 +0900

    三角形出力出来た！！！！！！！！

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -21,7 +21,7 @@ struct PixelShaderOutput {
 
 PixelShaderOutput main() {
 	PixelShaderOutput output;
-	output.color = float32_t4(1.0f, 1.0f, 1.0f, 1.0f);
+	output.color = float32_t4(1.0, 1.0, 1.0, 1.0);
 
 	return output;
 }

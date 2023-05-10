15682a2ab33cd9f008de02287e7cc057ecba4cfa
翼 茂木 <k022g0130@m.neec.ac.jp>
2023-05-10 14:04:49 +0900

    途中保存

---
+++ HeaderCpp/WindowsInitialization/WindowsInitialization.cpp
@@ -0,0 +1,87 @@
+#include "WindowsInitialization.h"
+
+void WindowsInitialization::Initialize() {
+	//出力ウィンドウへの文字出力
+	OutputDebugStringA("Hello,DirectX!\n");
+
+	////ウィンドウクラスを登録する
+	WNDCLASS wc{};
+	//ウィンドウプロシージャ
+	wc.lpfnWndProc = WindowProc;
+	//ウィンドウクラス名
+	wc.lpszClassName = L"CG2WindowClass";
+	//インスタンスハンドル
+	wc.hInstance = GetModuleHandle(nullptr);
+	//カーソル
+	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+
+	//ウィンドウクラスを登録する
+	RegisterClass(&wc);
+
+
+
+	//int 32_tを使うためにcstdintをインクルード
+	//クライアント領域のサイズ
+	//クライアント領域・・・ゲーム画面が映る領域のこと
+	const int32_t kClientWidth = 1280;
+	const int32_t kClientHeight = 720;
+
+
+	//ウィンドウサイズを表す構造体にクライアント領域を入れる
+	RECT wrc = { 0,0,kClientWidth ,kClientHeight };
+
+	//クライアント領域を元に実際のサイズにwrcを変更してもらう
+	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+
+	//ウィンドウの生成
+	HWND hwnd = CreateWindow(
+		wc.lpszClassName,		//利用するクラス名
+		L"CG2",					//タイトルバーの文字
+		WS_OVERLAPPEDWINDOW,	//よく見るウィンドウスタイル
+		CW_USEDEFAULT,			//表示X座標
+		CW_USEDEFAULT,			//表示Y座標
+		wrc.right - wrc.left,	//ウィンドウ横軸
+		wrc.bottom - wrc.top,	//ウィンドウ縦軸
+		nullptr,				//親ウィンドウハンドル
+		nullptr,				//メニューハンドル
+		wc.hInstance,			//インスタンスハンドル
+		nullptr);				//オプション
+
+
+	////エラー放置ダメ、ゼッタイ
+	////DebugLayer
+	//デバッグレイヤー・・・警告やエラーの出す機構のこと。色々な情報を収集する。
+	//						今までは違法な状態で動いていた。
+	//CreateWindowの直後で行う
+	// 
+	//DirectX12
+	//DXGI
+	//Debug Layer
+	//Graphics Driver
+	//GPU
+
+#ifdef _DEBUG
+	ID3D12Debug1* debugController = nullptr;
+	if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController)))) {
+		//デバッグレイヤーを有効化する
+		debugController->EnableDebugLayer();
+		//さらにGPU側でもチェックを行うようにする
+		debugController->SetEnableGPUBasedValidation(TRUE);
+
+	}
+
+
+#endif
+
+
+	//ウィンドウを表示する
+	ShowWindow(hwnd, SW_SHOW);
+
+	MSG msg{};
+
+
+	//使い方はこんな感じ
+	//出力にちゃんと「WSTRING」が出る
+	//Log(ConvertString(std::format(L"wstring\n", wstringValue)));
+
+}

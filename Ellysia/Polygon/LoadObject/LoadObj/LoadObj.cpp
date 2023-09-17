4c36a672bc89e6f864d1763a032a5bc895e0bac1
翼 茂木 <k022g0130@m.neec.ac.jp>
2023-09-18 00:47:24 +0900

    三角形を作る

---
+++ Ellysia/Polygon/LoadObject/LoadObj/LoadObj.cpp
@@ -0,0 +1,88 @@
+#include "LoadObj.h"
+
+ModelData LoadObjFile(const std::string& directoryPath, const std::string& fileName) {
+	//1.中で必要となる変数の宣言
+	//構築するModelData
+	ModelData modelData;
+	//位置
+	std::vector<Vector4> positions;
+	//法線
+	std::vector<Vector3> normals;
+	//テクスチャ座標
+	std::vector<Vector2> texcoords;
+	//ファイルから読んだ1行を格納するもの
+	std::string line;
+
+
+	//2.ファイルを開く
+	std::ifstream file(directoryPath + "/" + fileName);
+	assert(file.is_open());
+	 
+	//3.実際にファイルを読み、ModelDataを構築していく
+
+	//getline...streamから1行読んでstringに格納する
+	//istringstream...文字列を分解しながら読むためのクラス、空白を区切りとして読む
+	//objファイルの先頭にはその行の意味を示す識別子(identifier/id)が置かれているので、最初にこの識別子を読み込む
+
+	//v...頂点位置
+	//vt...頂点テクスチャ座標
+	//vn...頂点法線
+	//f...面
+
+	while (std::getline(file, line)) {
+		std::string identifier;
+		std::istringstream s(line);
+		//先頭の識別子を読む
+		s >> identifier;
+
+		//identifierに応じた処理
+		if (identifier == "v") {
+			Vector4 position;
+			//ex).  v 「1.0000」 「1.0000」 「-0.0000」
+			s >> position.x >> position.y >> position.z;
+			position.w = 1.0f;
+			positions.push_back(position);
+		}
+		else if (identifier == "vt") {
+			Vector2 texcoord;
+			s >> texcoord.x >> texcoord.y;
+			texcoords.push_back(texcoord);
+		}
+		else if (identifier == "vn") {
+			Vector3 normal;
+			s >> normal.x >> normal.y >> normal.z;
+			normals.push_back(normal);
+		}
+		else if (identifier == "f") {
+			//面は三角形限定。その他は未対応
+			for (int32_t faceVertex = 0; faceVertex < 3; ++faceVertex) {
+				std::string vertexDefinition;
+				s >> vertexDefinition;
+				//頂点の要素へのINdexは「位置/uv/法線」で格納されているので、分解してindexを取得する
+				std::istringstream v(vertexDefinition);
+				uint32_t elementIndices[3];
+				for (int32_t element = 0; element < 3; ++element) {
+					std::string index;
+					// 「/」区切りでインデックスを読んでいく
+					std::getline(v, index, '/');
+					elementIndices[element] = std::stoi(index);
+
+				}
+				//要素へのIndexから実際の要素の値を取得して、頂点を構築する
+				Vector4 position = positions[elementIndices[0] - 1];
+				Vector2 texcoord = texcoords[elementIndices[1] - 1];
+				Vector3 normal = normals[elementIndices[2] - 1];
+				VertexData vertex = { position,texcoord,normal };
+				modelData.vertices.push_back(vertex);
+
+
+			}
+		}
+
+
+
+	}
+
+
+	//4.ModelDataを返す
+}

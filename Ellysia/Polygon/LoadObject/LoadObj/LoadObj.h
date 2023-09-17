e6306ae65a94d37410d3e8a053152b9c5bb0158a
翼 茂木 <k022g0130@m.neec.ac.jp>
2023-09-17 20:53:29 +0900

    頂点情報を読み

--- Ellysia/Polygon/LoadObject/LoadObj/LoadObj.h
+++ Ellysia/Polygon/LoadObject/LoadObj/LoadObj.h
@@ -10,9 +10,9 @@ ModelData LoadObjFile(const std::string& directoryPath, const std::string& fileN
 	//構築するModelData
 	ModelData modelData;
 	//位置
-	std::vector<Vector4> position;
+	std::vector<Vector4> positions;
 	//法線
-	std::vector<Vector3> normal;
+	std::vector<Vector3> normals;
 	//テクスチャ座標
 	std::vector<Vector2> texcoords;
 	//ファイルから読んだ1行を格納するもの
@@ -24,5 +24,45 @@ ModelData LoadObjFile(const std::string& directoryPath, const std::string& fileN
 	assert(file.is_open());
 	 
 	//3.実際にファイルを読み、ModelDataを構築していく
+
+	//getline...streamから1行読んでstringに格納する
+	//istringstream...文字列を分解しながら読むためのクラス、空白を区切りとして読む
+	//objファイルの先頭にはその行の意味を示す識別子(identifier/id)が置かれているので、最初にこの識別子を読み込む
+
+	//v...頂点位置
+	//vt...頂点テクスチャ座標
+	//vn...頂点法線
+	//f...面
+
+	while (std::getline(file, line)) {
+		std::string identifier;
+		std::istringstream s(line);
+		//先頭の識別子を読む
+		s >> identifier;
+
+		//identifierに応じた処理
+		if (identifier == "v") {
+			Vector4 position;
+			s >> position.x >> position.y >> position.z;
+			position.w = 1.0f;
+			positions.push_back(position);
+		}
+		else if (identifier == "vt") {
+			Vector2 texcoord;
+			s >> texcoord.x >> texcoord.y;
+			texcoords.push_back(texcoord);
+		}
+		else if (identifier == "vn") {
+			Vector3 normal;
+			s >> normal.x >> normal.y >> normal.z;
+			normals.push_back(normal);
+		}
+
+
+
+
+	}
+
+
 	//4.ModelDataを返す
 }

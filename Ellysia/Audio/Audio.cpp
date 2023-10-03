db12d95243ed9ee43fc51da708b3601d7808f89e
翼 茂木 <k022g0130@m.neec.ac.jp>
2023-10-03 15:30:40 +0900

    音声の読み込み

--- Ellysia/Audio/Audio.cpp
+++ Ellysia/Audio/Audio.cpp
@@ -6,7 +6,15 @@ Audio::Audio() {
 }
 
 //初期化
+//これはDirecX初期化の後に入れてね
 void Audio::Initialize() {
+
+	//XAudioのエンジンのインスタンスを生成
+	HRESULT hr{};
+	hr = XAudio2Create(&xAudio2_, 0, XAUDIO2_DEFAULT_PROCESSOR);
+
+	//マスターボイスを生成
+	hr = xAudio2_->CreateMasteringVoice(&masterVoice_);
 
 }
 

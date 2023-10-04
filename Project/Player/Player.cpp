8268ed612e3febdb26d5e9df596b8ad7321eceac
翼 茂木 <k022g0130@m.neec.ac.jp>
2023-10-04 10:19:04 +0900

    Playerのクラス作った

---
+++ Project/Player/Player.cpp
@@ -0,0 +1,29 @@
+#include "Player.h"
+
+//コンストラクタ
+Player::Player() {
+
+}
+
+//初期化
+void Player::Initialize() {
+
+}
+
+//更新
+void Player::Update() {
+
+}
+
+//描画
+void Player::Draw() {
+
+}
+
+
+//デストラクタ
+Player::~Player() {
+
+}
+
+
---
+++ Project/Player/Player.h
@@ -0,0 +1,27 @@
+#pragma once
+
+class Player {
+public:
+	//コンストラクタ
+	Player();
+
+	//初期化
+	void Initialize();
+
+	//更新
+	void Update();
+
+	//描画
+	void Draw();
+
+
+	//デストラクタ
+	~Player();
+
+
+
+
+private:
+
+
+};

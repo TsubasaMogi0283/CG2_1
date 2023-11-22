2ee54905022d73f750dc7a38996d7d6eb139fef8
翼 茂木 <k022g0130@m.neec.ac.jp>
2023-11-22 10:37:23 +0900

    ホーミングよく分からなくなるので消した

--- Project/Enemy/Bullet/EnemyBullet.cpp
+++ Project/Enemy/Bullet/EnemyBullet.cpp
@@ -17,53 +17,17 @@ void EnemyBullet::Initialzie(Vector3 position, Vector3 velocity){
 	model_->CreateObject("Resources/Sample/cube", "cube.obj");
 
 	const float SCALE_ = 0.3f;
-	transform_.scale = {SCALE_,SCALE_,SCALE_*10.0f};
+	transform_.scale = {SCALE_,SCALE_,SCALE_*SCALE_};
 	transform_.translate = position;
 
 	velocity_ = velocity;
-
-
-	//アークタンジェント(atan2)の使い方
-	//1...高さ
-	//2...底辺
-
-
-	//Y軸回り角度(θy)
-	transform_.rotate.y = std::atan2(velocity_.x, velocity_.z);
-	//X軸回りの角度
-	//解法2の方が簡単だからこっちでやる
-	//XとZの2Dベクトル
-	float velocityXZ = sqrt(velocity_.x*velocity_.x + velocity_.z*velocity_.z);
-	float heightY = -velocity_.y;
-	//X軸回りの角度(θx)
-	transform_.rotate.x = std::atan2(heightY, velocityXZ);
 
 
 
 }
 
 void EnemyBullet::Update(){
-	//敵弾から自キャラ屁のベクトルを計算
-	Vector3 toPlayer = Subtract(player_->GetTranslate(),GetTranslate());
-
-	Normalize(toPlayer);
-	Normalize(velocity_);
-	//球面線形補間により、今の速度と自kたらへのベクトルを内挿し、新たな速度とする
-	//自分のだとかなり小さくしないとキツイと分かった。
-	velocity_ = Slerp(velocity_, toPlayer, 0.03f);
-
-
-
-	//Y軸回り角度(θy)
-	transform_.rotate.y = std::atan2(velocity_.x, velocity_.z);
-	//X軸回りの角度
-	//解法2の方が簡単だからこっちでやる
-	//XとZの2Dベクトル
-	float velocityXZ = sqrt(velocity_.x*velocity_.x + velocity_.z*velocity_.z);
-	float heightY = -velocity_.y;
-	//X軸回りの角度(θx)
-	transform_.rotate.x = std::atan2(heightY, velocityXZ);
-
+	
 
 
 	transform_.translate = Add(transform_.translate, velocity_);

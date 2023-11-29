572cb95eb42e6c14c1faf7c1d3ee4d5a1bf8ae06
翼 茂木 <k022g0130@m.neec.ac.jp>
2023-11-29 10:47:28 +0900

    衝突属性を持たせる

--- Project/Collider/Collider.h
+++ Project/Collider/Collider.h
@@ -12,6 +12,9 @@ public:
 	virtual Vector3 GetWorldPosition() = 0;
 
 
+public:
+	//アクセッサ
+
 	//半径を取得
 	float GetRadius() {
 		return radius_;
@@ -21,9 +24,34 @@ public:
 		this->radius_ = radius;
 	}
 
+public:
+	//衝突属性(自分)を取得
+	uint32_t GetCollisionAttribute() {
+		return collisionAttribute_;
+	}
+	//衝突属性(自分)を設定
+	void SetCollisionAttribute(uint32_t collisionAttribute) {
+		this->collisionAttribute_ = collisionAttribute;
+	}
+
+	//衝突マスク(相手)を取得
+	uint32_t GetCollisionMask() {
+		return collisionMask_;
+	}
+	//衝突マスク(相手)を設定
+	void SetCollisionMask(uint32_t collisionMask) {
+		this->collisionMask_ = collisionMask;
+	}
+
 private:
 	//衝突半径
 	float radius_ = 1.0f;
+
+	//衝突判定(自分)
+	uint32_t collisionAttribute_ = 0xffffffff;
+
+	//衝突判定(相手)
+	uint32_t collisionMask_ = 0xffffffff;
 
 };
 

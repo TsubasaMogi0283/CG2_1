e5374dcfa485d21c2c74941b89747705fafa2f89
翼 茂木 <k022g0130@m.neec.ac.jp>
2023-12-06 15:43:42 +0900

    01_03_確認課題

--- Ellysia/Math/Quaternion/Quaternion.cpp
+++ Ellysia/Math/Quaternion/Quaternion.cpp
@@ -3,13 +3,38 @@
 
 Quaternion Multiply(const Quaternion& lhs, const Quaternion& rhs){
     Quaternion result = {};
+    Quaternion q = lhs;
+    Quaternion r = rhs;
     
-    return Quaternion();
+    
+    //wはそのまま
+    //xはi部分
+    //yはj部分
+    //zはk部分
+
+    result.w = q.w * r.w - q.x * r.x - q.y * r.y - q.z * r.z;
+    result.x = q.y * r.z - q.z * r.y + r.w * q.x + q.w * r.x;
+    result.y = q.z * r.x - q.x * r.z + r.w * q.y + q.w * r.y;
+    result.z = q.x * r.y - q.y * r.x + r.w * q.z + q.w * r.z;
+    
+
+
+    return result;
 }
 
+//単位
 Quaternion IdentityQuaternion(){
+
+    //e=1+0i+0j+0k
+    //qe=eq=q
+
     Quaternion result = {};
 
+    result.w = 1.0f;
+    result.x = 0.0f;
+    result.y = 0.0f;
+    result.z = 0.0f;
+    
 
 
 
@@ -17,12 +42,32 @@ Quaternion IdentityQuaternion(){
     return result;
 }
 
+//共役
 Quaternion Conjugate(const Quaternion& quaternion){
-    return Quaternion();
+    
+    //q*=w-xi-yj-zk=(qw,-qv)
+    //虚数部分があるところはマイナスになるよ
+    Quaternion result = {};
+    result.x = -quaternion.x;
+    result.y = -quaternion.y;
+    result.z = -quaternion.z;
+    result.w = quaternion.w;
+
+
+    return result;
 }
 
 float Norm(const Quaternion& quaternion){
-    return 0.0f;
+
+    //||q||=sqrtf(qq*)
+    float result = 0.0f;
+    result = sqrtf(
+        quaternion.w * quaternion.w +
+        quaternion.x * quaternion.x +
+        quaternion.y * quaternion.y +
+        quaternion.z * quaternion.z);
+     
+    return result;
 }
 
 Quaternion Normalize(const Quaternion& quaternion){
@@ -61,6 +106,23 @@ Quaternion Normalize(const Quaternion& quaternion){
 }
 
 Quaternion Inverse(const Quaternion& quaternion){
+    //q^-1=q*
+    //   ||q||^2
 
-    return Quaternion();
+
+    Quaternion result = {};
+
+
+    float norm = Norm(quaternion);
+    Quaternion conjugate = Conjugate(quaternion);
+
+    float t = norm * norm;
+
+    result.x = conjugate.x / t;
+    result.y = conjugate.y / t;
+    result.z = conjugate.z / t;
+    result.w = conjugate.w / t;
+
+
+    return result;
 }

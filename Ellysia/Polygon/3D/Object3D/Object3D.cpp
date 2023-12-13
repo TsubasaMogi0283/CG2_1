87d612bc2d9f7318301992b1c29a7beee22ca5f8
翼 茂木 <k022g0130@m.neec.ac.jp>
2023-12-13 16:29:02 +0900

    1

--- Ellysia/Polygon/3D/Object3D/Object3D.cpp
+++ Ellysia/Polygon/3D/Object3D/Object3D.cpp
@@ -1,1 +1,13 @@
 #include "Object3D.h"
+
+Object3D::Object3D(){
+
+}
+
+void Object3D::StaticInitialize(){
+
+}
+
+Object3D::~Object3D(){
+
+}
--- Ellysia/Polygon/3D/Object3D/Object3D.h
+++ Ellysia/Polygon/3D/Object3D/Object3D.h
@@ -1,5 +1,22 @@
 #pragma once
 class Object3D{
+public:
+	
+	Object3D();
 
+
+	static void StaticInitialize();
+
+	void Update();
+
+
+	void Draw();
+
+
+
+	~Object3D();
+
+
+private:
 };
 

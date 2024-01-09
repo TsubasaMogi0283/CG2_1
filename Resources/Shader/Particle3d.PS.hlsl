3930cbb8369040e31bc5c89ae5621f0baa2513c4
翼 茂木 <k022g0130@m.neec.ac.jp>
2024-01-09 14:14:49 +0900

    1

--- Resources/Shader/Particle3d.PS.hlsl
+++ Resources/Shader/Particle3d.PS.hlsl
@@ -18,18 +18,18 @@
 //Material...色など三角形の表面の材質をけっていするもの
 struct Material
 {
-    float32_t4 color;
-    int32_t enableLighting; ///
-    float32_t4x4 uvTransform;
+    float4 color;
+    int enableLighting; ///
+    float4x4 uvTransform;
 };
 
 
 struct DirectionalLight
 {
 	//ライトの色
-    float32_t4 color;
+    float4 color;
 	//ライトの向き
-    float32_t3 direction;
+    float3 direction;
 	//ライトの輝度
     float intensity;
 };
@@ -43,7 +43,7 @@ struct DirectionalLight
 ConstantBuffer<Material> gMaterial : register(b0);
 //出来たらgDirectinalLightとそれに関するRootSignatureも変えてね
 ConstantBuffer<DirectionalLight> gDirectionalLight : register(b1);
-Texture2D<float32_t4> gTexture : register(t0);
+Texture2D<float4> gTexture : register(t0);
 SamplerState gSampler : register(s0);
 
 //Textureは基本的にそのまま読まずSamplerを介して読む
@@ -55,7 +55,7 @@ SamplerState gSampler : register(s0);
 
 struct PixelShaderOutput
 {
-    float32_t4 color : SV_TARGET0;
+    float4 color : SV_TARGET0;
 };
 
 
@@ -67,7 +67,7 @@ PixelShaderOutput main(VertexShaderOutput input)
 	
 	//Materialを拡張する
     float4 transformedUV = mul(float32_t4(input.texcoord, 0.0f, 1.0f), gMaterial.uvTransform);
-    float32_t4 textureColor = gTexture.Sample(gSampler, transformedUV.xy);
+    float4 textureColor = gTexture.Sample(gSampler, transformedUV.xy);
 
     output.color = gMaterial.color * textureColor;
     if (output.color.a == 0.0f)

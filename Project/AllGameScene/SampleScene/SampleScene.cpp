34aaa60069a67da34d9f48e7c3068fd575b5cab8
翼 茂木 <k022g0130@m.neec.ac.jp>
2024-01-30 16:43:26 +0900

    csv調整した

--- Project/AllGameScene/SampleScene/SampleScene.cpp
+++ Project/AllGameScene/SampleScene/SampleScene.cpp
@@ -43,67 +43,6 @@ void SampleScene::LoadEnemyPopData() {
 	file.close();
 }
 
-//敵発生コマンドの更新
-void SampleScene::UpdateEnemyPopCommands() {
-	//1行分の文字列を入れる変数
-	std::string line;
-
-	//コマンド実行ループ
-	while (getline(enemyPopCommands_, line)) {
-		//1行分の文字列をストリームに変呼応して解析しやすくする
-		std::istringstream line_stream(line);
-
-		std::string word;
-		//「,」区切りの先頭文字列を取得
-		getline(line_stream, word, ',');
-
-
-		//"//"から始まる行はコメント
-		if (word.find("//") == 0) {
-			//コメント行を飛ばす
-			continue;
-		}
-
-		//POPコマンド
-		if (word.find("POP") == 0) {
-			//x座標
-			getline(line_stream, word, ',');
-			float x = (float)std::atof(word.c_str());
-
-			//y座標
-			getline(line_stream, word, ',');
-			float y = (float)std::atof(word.c_str());
-
-			//z座標
-			getline(line_stream, word, ',');
-			float z = (float)std::atof(word.c_str());
-
-			//敵を発生させる
-			GenerateEnemy({ x,y,z });
-
-		}
-		else if (word.find("WAIT") == 0) {
-			getline(line_stream, word, ',');
-
-			//待ち時間
-			int32_t waitTime = atoi(word.c_str());
-
-			//待機開始
-			isWait_ = true;
-			//待機タイマー
-			waitingTimer_ = waitTime;
-
-
-
-			//コマンドループを抜ける
-			break;
-
-
-		}
-
-
-	}
-}
 
 /// <summary>
 /// 初期化
@@ -197,6 +136,77 @@ void SampleScene::CheckAllCollisions() {
 
 }
 
+//敵発生コマンドの更新
+void SampleScene::UpdateEnemyPopCommands() {
+
+	//待機処理
+	if (isWait_ == true) {
+		waitingTimer_--;
+		if (waitingTimer_ <= 0) {
+			//待機完了
+			isWait_ = false;
+		}
+	}
+
+	//1行分の文字列を入れる変数
+	std::string line;
+
+	//コマンド実行ループ
+	while (getline(enemyPopCommands_, line)) {
+		//1行分の文字列をストリームに変呼応して解析しやすくする
+		std::istringstream line_stream(line);
+
+		std::string word;
+		//「,」区切りの先頭文字列を取得
+		getline(line_stream, word, ',');
+
+
+		//"//"から始まる行はコメント
+		if (word.find("//") == 0) {
+			//コメント行を飛ばす
+			continue;
+		}
+
+		//POPコマンド
+		if (word.find("POP") == 0) {
+			//x座標
+			getline(line_stream, word, ',');
+			float x = (float)std::atof(word.c_str());
+
+			//y座標
+			getline(line_stream, word, ',');
+			float y = (float)std::atof(word.c_str());
+
+			//z座標
+			getline(line_stream, word, ',');
+			float z = (float)std::atof(word.c_str());
+
+			//敵を発生させる
+			GenerateEnemy({ x,y,z });
+
+		}
+		else if (word.find("WAIT") == 0) {
+			getline(line_stream, word, ',');
+
+			//待ち時間
+			int32_t waitTime = atoi(word.c_str());
+
+			//待機開始
+			isWait_ = true;
+			//待機タイマー
+			waitingTimer_ = waitTime;
+
+
+
+			//コマンドループを抜ける
+			break;
+
+
+		}
+
+
+	}
+}
 
 
 

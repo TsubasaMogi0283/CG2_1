0602ef56684924401b6f4c91410833a077b90bf6
翼 茂木 <k022g0130@m.neec.ac.jp>
2024-01-31 11:30:44 +0900

    空っぽのFollowCameraクラスを作った

---
+++ Project/Object/FollowCamera/FollowCamera.cpp
@@ -0,0 +1,29 @@
+#include "FollowCamera.h"
+
+/// <summary>
+/// コンストラクタ
+/// </summary>
+FollowCamera::FollowCamera() {
+
+}
+
+/// <summary>
+/// 初期化
+/// </summary>
+void FollowCamera::Initialize() {
+
+}
+
+/// <summary>
+/// 更新
+/// </summary>
+void FollowCamera::Update() {
+
+}
+
+/// <summary>
+/// デストラクタ
+/// </summary>
+FollowCamera::~FollowCamera() {
+
+}
---
+++ Project/Object/FollowCamera/FollowCamera.h
@@ -0,0 +1,32 @@
+#pragma once
+
+//レールとカメラオブジェクトも２つの役割を持つよ
+class FollowCamera{
+public:
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
+	FollowCamera();
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	~FollowCamera();
+
+private:
+
+
+
+
+};
+

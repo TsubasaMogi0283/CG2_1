497e396a377722b5daf9a741f4d487545629e8c0
翼 茂木 <k022g0130@m.neec.ac.jp>
2024-02-22 17:26:26 +0900

    Assimp出来た！！

--- Ellysia/Polygon/3D/LoadModelData/LoadModelData.cpp
+++ Ellysia/Polygon/3D/LoadModelData/LoadModelData.cpp
@@ -1,24 +1,26 @@
 #include "LoadModelData.h"
 #include <cassert>
 
+
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+
 //モデルデータの読み込み
 ModelData LoadObjectFile(const std::string& directoryPath, const std::string& fileName) {
 	//1.中で必要となる変数の宣言
 	ModelData modelData;
-	//位置
-	std::vector<Vector4> positions;
-	//法線
-	std::vector<Vector3> normals;
-	//テクスチャ座標
-	std::vector<Vector2> texcoords;
-	//ファイルから読んだ1行を格納するもの
-	std::string line;
 
 
+	//assimpでobjを読む
+	//assimpを利用してしてobjファイルを読んでいく
+	Assimp::Importer importer;
+	std::string filePath = directoryPath + "/" + fileName;
+	const aiScene* scene = importer.ReadFile(filePath.c_str(), aiProcess_FlipWindingOrder | aiProcess_FlipUVs);
+	//メッシュがないのは対応しない
+	assert(scene->HasMeshes());
 
-	//2.ファイルを開く
-	std::ifstream file(directoryPath + "/" + fileName);
-	assert(file.is_open());
 
 	//3.実際にファイルを読み、ModelDataを構築していく
 
@@ -26,91 +28,58 @@ ModelData LoadObjectFile(const std::string& directoryPath, const std::string& fi
 	//istringstream...文字列を分解しながら読むためのクラス、空白を区切りとして読む
 	//objファイルの先頭にはその行の意味を示す識別子(identifier/id)が置かれているので、最初にこの識別子を読み込む
 
-	//v...頂点位置
-	//vt...頂点テクスチャ座標
-	//vn...頂点法線
-	//f...面
 
-	while (std::getline(file, line)) {
-		std::string identifier;
-		std::istringstream s(line);
-		//先頭の識別子を読む
-		s >> identifier;
+	//Meshを解析
+	//Meshは複数のFaceで構成され、そのFaceは複数の頂点で構成されている
+	//さらにSceneには複数のMeshが存在しているというわけであるらしい
+	for (uint32_t meshIndex = 0; meshIndex < scene->mNumMeshes; ++meshIndex) {
+		aiMesh* mesh = scene->mMeshes[meshIndex];
+		//Normalなので法線がない時は止める
+		assert(mesh->HasNormals());
+		//TextureCoordsなのでTexCoordが無い時は止める
+		assert(mesh->HasTextureCoords(0));
 
-		//identifierに応じた処理
-		if (identifier == "v") {
-			Vector4 position;
-			//ex).  v 「1.0000」 「1.0000」 「-0.0000」
-			s >> position.x >> position.y >> position.z;
-			position.z *= -1.0f;
-			position.w = 1.0f;
-			positions.push_back(position);
-		}
-		else if (identifier == "vt") {
-			Vector2 texcoord;
-			s >> texcoord.x >> texcoord.y;
-			texcoord.y = 1.0f - texcoord.y;
-			texcoords.push_back(texcoord);
-		}
-		else if (identifier == "vn") {
-			Vector3 normal;
-			s >> normal.x >> normal.y >> normal.z;
-			normal.z *= -1.0f;
-			normals.push_back(normal);
-		}
-		else if (identifier == "f") {
-			//面は三角形限定。その他は未対応
-			VertexData triangle[3];
-			for (int32_t faceVertex = 0; faceVertex < 3; ++faceVertex) {
-				std::string vertexDefinition;
-				s >> vertexDefinition;
-				//頂点の要素へのINdexは「位置/uv/法線」で格納されているので、分解してindexを取得する
-				std::istringstream v(vertexDefinition);
-				uint32_t elementIndices[3];
-
-
-				for (int32_t element = 0; element < 3; ++element) {
-					std::string index;
-					// 「/」区切りでインデックスを読んでいく
-					std::getline(v, index, '/');
-					elementIndices[element] = std::stoi(index);
-
-
-				}
-				//要素へのIndexから実際の要素の値を取得して、頂点を構築する
-				Vector4 position = positions[elementIndices[0] - 1];
-				Vector2 texcoord = texcoords[elementIndices[1] - 1];
-				Vector3 normal = normals[elementIndices[2] - 1];
-				//VertexData vertex = { position,texcoord,normal };
-				//modelData.vertices.push_back(vertex);
-
-				triangle[faceVertex] = { position,texcoord,normal };
+		//faceを解析する
+		for (uint32_t faceIndex = 0; faceIndex < mesh->mNumFaces;++faceIndex ) {
+			aiFace& face = mesh->mFaces[faceIndex];
+			//三角形のみサポート
+			assert(face.mNumIndices == 3);
+			//ここからFaceの中身であるVertexの解析を行っていく
+			for (uint32_t element = 0; element < face.mNumIndices; ++element) {
+				uint32_t vertexIndex = face.mIndices[element];
+				aiVector3D& position = mesh->mVertices[vertexIndex];
+				aiVector3D& normal = mesh->mNormals[vertexIndex];
+				aiVector3D& texcoord = mesh->mTextureCoords[0][vertexIndex];
+				VertexData vertex;
+				vertex.position = { position.x,position.y,position.z,1.0f };
+				vertex.normal = { normal.x,normal.y,normal.z };
+				vertex.texCoord = { texcoord.x,texcoord.y };
+				//aiProcess_MakeLeftHandedはz*=-1で、
+				vertex.position.x *= -1.0f;
+				vertex.normal.x *= -1.0f;
+				modelData.vertices.push_back(vertex);
 
 
 
 			}
-			//頂点を逆順で登録することで、回り順を逆にする
-			modelData.vertices.push_back(triangle[2]);
-			modelData.vertices.push_back(triangle[1]);
-			modelData.vertices.push_back(triangle[0]);
+
 
 		}
-		else if (identifier == "mtllib") {
-			//materialTemplateLibraryファイルの名前を取得する
-			std::string materialFileName;
-			s >> materialFileName;
-			//基本的にobjファイルと同一階層にmtlは存在させるので、ディレクトリ名とファイル名を渡す
-			modelData.material = LoadMaterialTemplateFile(directoryPath, materialFileName);
-		}
-
 
 	}
 
 
+	//Materialを解析する
+	for (uint32_t materialIndex = 0; materialIndex < scene->mNumMaterials; ++materialIndex) {
+		aiMaterial* material = scene->mMaterials[materialIndex];
+		if (material->GetTextureCount(aiTextureType_DIFFUSE) != 0) {
+			aiString textureFilePath;
+			material->GetTexture(aiTextureType_DIFFUSE, 0, &textureFilePath);
+			modelData.material.textureFilePath = directoryPath + "/" + textureFilePath.C_Str();
+		}
+	}
 
-
-
-	//4.ModelDataを返す
+	//ModelDataを返す
 	return modelData;
 }
 

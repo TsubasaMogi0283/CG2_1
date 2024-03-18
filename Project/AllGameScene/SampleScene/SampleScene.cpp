9f481fedcc9503ec39bbfc089fd4bb9c8b342091
翼 茂木 <k022g0130@m.neec.ac.jp>
2024-03-18 09:29:07 +0900

    1

--- Project/AllGameScene/SampleScene/SampleScene.cpp
+++ Project/AllGameScene/SampleScene/SampleScene.cpp
@@ -1,13 +1,7 @@
 #include "SampleScene.h"
-#include "GameManager.h"
-#include "TextureManager.h"
-#include <imgui.h>
-#include <Input.h>
-#include <Camera.h>
-#include "Audio.h"
 
-#include "SampleScene2/SampleScene2.h"
-#include <Polygon/3D/Particle3DManager/Particle3DManager.h>
+#include <list>
+
 
 /// <summary>
 	/// コンストラクタ
@@ -22,119 +16,17 @@ SampleScene::SampleScene() {
 /// 初期化
 /// </summary>
 void SampleScene::Initialize() {
-
-	uint32_t modelData = ModelManager::GetInstance()->LoadObject("Resources/CG3/terrain", "terrain.obj");
-
-
-	uint32_t model1 = ModelManager::GetInstance()->LoadObject("Resources/CG3/terrain", "terrain.obj");
-	uint32_t model2 = ModelManager::GetInstance()->LoadObject("Resources/CG3/Sphere", "Sphere.obj");
-
-	for (int i = 0; i < MODEL_AMOUNT_; i++) {
-		model_[0].reset(Model::Create(model1));
-		model_[1].reset(Model::Create(model2));
-		modelWorldTransform_[i].Initialize();
-	}
-
-	const float SCALE_SIZE = 1.0f;
-	modelWorldTransform_[0].scale_ = modelScale_;
-	modelWorldTransform_[1].scale_ = modelScale_;
-
-
-
-	modelWorldTransform_[1].translate_ = { 0.0f,-1.0f,0.0f };
-
-
-	sprite = std::make_unique<Sprite>();
-	uint32_t textureHandle = TextureManager::LoadTexture("Resources/uvChecker.png");
-	spritePosition_ = { 100.0f,100.0f };
-	sprite.reset(Sprite::Create(textureHandle, spritePosition_));
-
-	
-
-	uint32_t particleModel= ModelManager::GetInstance()->LoadObject("Resources/05_02", "plane.obj");
-	particle_.reset(Particle3D::Create(particleModel));
-	int count = 3;
-	
-
-
-
-	//0.5秒ごとに発生
-	float frequency = 0.5f;
-	//発生頻度用の時刻。0.0で初期化
-	float frequencyTime = 0.0f;
-
-
-
-	particleTextureHandle_ = TextureManager::GetInstance()->LoadTexture("Resources/CG3/circle.png");
-
-	accelerationField_.acceleration = { 5.0f,0.0f,0.0, };
-	accelerationField_.area.min = { -1.0f,-1.0f,-1.0f };
-	accelerationField_.area.max = { 1.0f,1.0f,1.0f };
-
-	isSetField_ = true;
-	particle_->SetField(isSetField_);
-	particle_->SetAccelerationField(accelerationField_);
-
-	particle_->SetCount(count);
-	particle_->SetFrequency(frequency);
-	particle_->SetFrequencyTime(frequencyTime);
-	particleTranslate_ = { 0.0f,0.0f,0.0f };
-
-
-
-
-
-	particle2_.reset(Particle3D::Create(particleModel));
-	particleTextureHandle2_ = TextureManager::GetInstance()->LoadTexture("Resources/uvChecker.png");
-	particleTranslate2_ = { 0.0f,9.0f,0.0f };
-	accelerationField2_.acceleration = { 5.0f,0.0f,0.0, };
-	accelerationField2_.area.min = { -1.0f,-1.0f,-1.0f };
-	accelerationField2_.area.max = { 1.0f,1.0f,1.0f };
-	isSetField2_ = false;
-
-
-
-	cameraTranslate_ = { 0.0f,10.0f,-30.0f };
-	camera_.Initialize();
-	camera_.rotate_ = { 0.2f,0.0f,0.0f };
-	camera_.translate_ = cameraTranslate_;
-
-
-	Particle3DManager::GetInstance()->GetInstance()->Initialize();
-	
-	particleTextureHandle_ = TextureManager::GetInstance()->LoadTexture("Resources/CG3/circle.png");
-	uint32_t bonusTextureHandle = TextureManager::GetInstance()->LoadTexture("Resources/Sample/Ellysia/Ellysia.png");
-
-	//これEmitterクラス作らなくて良くない？
-	Particle3DManager::GetInstance()->Emit("sample", particleTextureHandle_, { 4.0f,4.0f,0.0f }, 5);
-	Particle3DManager::GetInstance()->Emit("sample2", particleTextureHandle2_, { 0.0f,7.0f,0.0f }, 2);
-	
-
-
-	//同じ場所
-	Particle3DManager::GetInstance()->Emit("sample3", particleTextureHandle_, { 0.0f,0.0f,0.0f }, 20);
-	Particle3DManager::GetInstance()->Emit("sample4", particleTextureHandle2_, { 0.0f,0.0f,0.0f }, 5);
-
-	Particle3DManager::GetInstance()->Emit("sample5", bonusTextureHandle, { -2.0f,0.0f,0.0f }, 3);
-
-
-
-	player_ = std::make_unique<Player>();
-	player_->Initialize();
-
-
-
-
-
-
-
-
-
 	audio_ = Audio::GetInstance();
-	uint32_t audioHandle_ = audio_->LoadWave("Resources/Audio/Sample/The_Land_Of_Demise_32.wav");
+	audioHandle_ = audio_->LoadWave("Resources/Audio/Sample/Win.wav");
 	audio_->PlayWave(audioHandle_, true);
-
+	audio_->SetPan(audioHandle_, pan_);
+	audio_->ChangePitch(audioHandle_, pitch_);
 }
+
+
+
+
+
 
 /// <summary>
 /// 更新
@@ -142,120 +34,16 @@ void SampleScene::Initialize() {
 void SampleScene::Update(GameManager* gameManager) {
 
 
-	model_[0]->SetColor(modelColor_);
-	//model_[0]->SetDirectionalLightIntensity(intensity_);
-	//model_[1]->SetDirectionalLightIntensity(intensity_);
-	//model_[0]->SetDirectionalLightDirection(lightingDirection_);
-	//model_[1]->SetDirectionalLightDirection(lightingDirection_);
-
-	
-
-	const float SPEED = 1.0f;
-	
-
-	/*pointLightPosition_.x += move.x;
-	pointLightPosition_.z += move.y;
-	if (pointLightPosition_.x > 15.0f ||
-		pointLightPosition_.x < -15.0f){
-		move.x *= -1.0f;
-	}
-	if (pointLightPosition_.z > 10.0f ||
-		pointLightPosition_.z < -10.0f) {
-		move.y *= -1.0f;
-	}*/
+	audio_->ChangePitch(audioHandle_, pitch_);
+	audio_->SetPan(audioHandle_, pan_);
 
 
-	for (int i = 0; i < MODEL_AMOUNT_; i++) {
-		model_[i]->SetSpotLightPosition(spotLightPosition_);
-		model_[i]->SetSpotLightIntensity(spotLightIntensity_);
-		model_[i]->SetSpotLightColor(spotLightColor_);
-		model_[i]->SetSpotLightDecay(spotLightDecay_);
-		model_[i]->SetSpotLightDistance(spotLightDistance_);
-		model_[i]->SetSpotLightDirection(spotLightDirection_);
-		model_[i]->SetSpotLightCosAngle(cosAngle_);
-		model_[i]->SetCosFalloffStart(cosFalloffStart_);
-
-	}
-
-
-
-	particle_->SetTranslate(particleTranslate_);
-	particle2_->SetTranslate(particleTranslate2_);
-
-
-	particle_->SetField(isSetField_);
-
-
-	sprite->SetPosition(spritePosition_);
-
-	const float MOVE_AMOUNT=1.0f;
-
-
-	const float CAMERA_VELOCITY = 0.05f;
-	if (Input::GetInstance()->IsPushKey(DIK_RIGHT) == true) {
-		camera_.translate_.x += MOVE_AMOUNT * CAMERA_VELOCITY;
-	}
-	if (Input::GetInstance()->IsPushKey(DIK_LEFT) == true) {
-		camera_.translate_.x -= MOVE_AMOUNT * CAMERA_VELOCITY;
-	}
-	if (Input::GetInstance()->IsPushKey(DIK_UP) == true) {
-		camera_.translate_.y += MOVE_AMOUNT * CAMERA_VELOCITY;
-	}
-	if (Input::GetInstance()->IsPushKey(DIK_DOWN) == true) {
-		camera_.translate_.y -= MOVE_AMOUNT * CAMERA_VELOCITY;
-	}
-
-	player_->Update();
-	
-	//ウィンドウサイズの設定は↓でやるよ
 #ifdef _DEBUG
-	ImGui::Begin("Model");
-	ImGui::SliderFloat("Intensity", &spotLightIntensity_, 0.0f, 3.0f);
-	ImGui::SliderFloat3("spotLightPosition_", &spotLightPosition_.x, -10.0f, 10.0f);
-	ImGui::SliderFloat3("spotLightDirection_",&spotLightDirection_.x,-1.0f,1.0f);
-	ImGui::SliderFloat("spotLightDecay_",&spotLightDecay_,0.0f,30.0f);
-	ImGui::SliderFloat("spotLightDistance_", &spotLightDistance_, 0.0f, 30.0f);
-	ImGui::SliderFloat("cosFalloffStart_", &cosFalloffStart_, 0.0f, 1.0f);
-	ImGui::SliderFloat("cosAngle_", &cosAngle_, 0.0f, 1.0f);
-
-
-
-
-
+	ImGui::Begin("Audio");
+	ImGui::SliderFloat("Pan", &pan_, -1.0f, 1.0f);
+	ImGui::InputInt("Pitch", &pitch_);
 	ImGui::End();
-
-	ImGui::Begin("Camera");
-	ImGui::SliderFloat3("Rotate", &camera_.rotate_.x, -3.0f, 3.0f);
-	ImGui::SliderFloat3("Position", &camera_.translate_.x, -40.0f, 40.0f);
-	ImGui::End();
-
-
-	ImGui::SetNextWindowSize(ImVec2(500, 100));
-	ImGui::Begin("Sprite");
-	ImGui::SliderFloat2("Position", &spritePosition_.x, 0.0f, 500.0f, "%.1f");
-	ImGui::End();
-
 #endif
-
-	for (int i = 0; i < MODEL_AMOUNT_; i++) {
-		modelWorldTransform_[i].Update();
-	}
-	camera_.Update();
-
-
-
-	if (Input::GetInstance()->IsTriggerKey(DIK_SPACE) == true) {
-		
-		audio_->StopWave(audioHandle2_);
-		isEnd_ = true;
-	}
-
-	if (isEnd_ == true) {
-		audio_->StopWave(audioHandle2_);
-		gameManager->ChangeScene(new SampleScene2());
-	}
-
-
 
 }
 
@@ -263,21 +51,15 @@ void SampleScene::Update(GameManager* gameManager) {
 /// 描画
 /// </summary>
 void SampleScene::Draw() {
-	for (int i = 0; i < MODEL_AMOUNT_; i++) {
-		//model_[i]->Draw(modelWorldTransform_[i], camera_);
-	
-	}
-	//particle_->Draw(particleTextureHandle_,camera_);
-	//particle2_->Draw(particleTextureHandle2_, camera_);
 
-	player_->Draw(camera_);
-
-	Particle3DManager::GetInstance()->Draw(camera_);
-	//sprite->Draw();
 }
+
+
+
 
 /// <summary>
 /// デストラクタ
 /// </summary>
 SampleScene::~SampleScene() {
+
 }
--- Project/AllGameScene/SampleScene/SampleScene.h
+++ Project/AllGameScene/SampleScene/SampleScene.h
@@ -1,20 +1,28 @@
 #pragma once
-#include "IGameScene.h"
+#include "AllGameScene/GameManager/IGameScene.h"
 
 #include "Sprite.h"
-#include "Transform.h"
+#include "GameManager.h"
+#include "Model.h"
+#include "TextureManager.h"
+#include "Camera.h"
 
-#include "Particle3D.h"
-#include "Audio.h"
-#include <memory>
-#include <Model.h>
-#include <numbers>
 #include <Object/Player/Player.h>
+#include <Object/Enemy/Enemy.h>
+#include <Object/Skydome/Skydome.h>
+#include <Object/Collider/CollisionManager.h>
+#include <Object/RailCamera/RailCamera.h>
+
+#include <memory>
+#include <Line/Line.h>
+#include <Particle3D.h>
+#include <Audio.h>
+
 
 //StatePatternを使う時は必ず前方宣言をするように
-class GameManager;
+class Gamemanager;
 
-class SampleScene : public IGameScene{
+class SampleScene : public IGameScene {
 public:
 
 	//コンストラクタ
@@ -32,71 +40,15 @@ public:
 	/// 描画
 	void Draw()override;
 
+
+
 private:
-	static const int MODEL_AMOUNT_ = 2;
-
-	std::unique_ptr<Model> model_[MODEL_AMOUNT_] = { nullptr };
-	WorldTransform modelWorldTransform_[MODEL_AMOUNT_] = {};
-
-	Vector2 position_ = { 0.0f,0.0f };
-	Vector2 scale_ = { 1.0f,1.0f };
-	float rotate = 0.0f;
-	float intensity_ = 3.0f;
-	Vector3 lightingDirection_ = { 0.0f,-1.0f,0.0f };
-	Vector3 modelScale_ = { 3.0f,3.0f,3.0f };
-	Vector3 pointLightPosition_ = { 0.0f,3.0f,0.0f };
-	Vector4 pointLightColor_ = { 1.0f,1.0f,1.0f,1.0f };
-	float pointLightRadius_ = 6.0f;
-	float pointLightDecay_ = 0.0f;
-
-	Vector2 move = {1.0f,1.0f};
-
-
-
-	//
-	Vector4 spotLightColor_ = { 1.0f,1.0f,1.0f,1.0f };
-	Vector3 spotLightPosition_ = { 2.0f,1.25f,0.0f };
-	float spotLightDistance_ = 7.0f;
-	Vector3 spotLightDirection_ = { -1.0f,1.0f,0.0f };
-	float spotLightIntensity_ = 4.0f;
-	float spotLightDecay_ = 2.0f;
-	float cosAngle_ = std::cos(std::numbers::pi_v<float> / 3.0f);
-	float cosFalloffStart_ = 0.2f;
-
-	std::unique_ptr<Sprite> sprite = nullptr;
-	Vector4 color_ = { 1.0f,1.0f,1.0f,1.0f };
-	Vector4 modelColor_ = { 1.0f,1.0f,1.0f,1.0f };
-	Vector2 spritePosition_ = {};
-
-	//パーティクル
-	std::unique_ptr<Particle3D> particle_ = nullptr;
-	uint32_t particleTextureHandle_ = 0u;
-	Vector3 particleTranslate_ = {};
-	AccelerationField accelerationField_ = {};
-	bool isSetField_ = false;
-
-
-	std::unique_ptr<Particle3D> particle2_ = nullptr;
-	uint32_t particleTextureHandle2_ = 0u;
-	Vector3 particleTranslate2_ = {};
-	AccelerationField accelerationField2_ = {};
-	bool isSetField2_ = false;
-
-
-
-	std::unique_ptr<Player> player_ = nullptr;
-
-
-
-	Camera camera_ = {};
-	Vector3 cameraTranslate_ = {0.0f,0.0f,0.0f};
-	
 
 	Audio* audio_ = nullptr;
-	uint32_t audioHandle2_ = 0u;
-	bool isEnd_ = false;
+	uint32_t audioHandle_ = 0u;
+
+	float_t pan_ = 0.0f;
+	int pitch_ = 0;
 };
-
-
 
 
